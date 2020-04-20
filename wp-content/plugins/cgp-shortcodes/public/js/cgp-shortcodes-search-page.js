a293a595ec47a7fa5c582b7445c6e09a38e25579
Pascal Vautour <vautour.pascal@gmail.com>
2020-04-20 17:41:08 -0400

    A solid base for filtering

--- wp-content/plugins/cgp-shortcodes/public/js/cgp-shortcodes-search-page.js
+++ wp-content/plugins/cgp-shortcodes/public/js/cgp-shortcodes-search-page.js
@@ -1,27 +1,62 @@
 (function () {
+  var cgpPill = Vue.component("cgp-pill", {
+    template:
+      '<button class="badge badge-pill badge-primary">\
+    {{ fieldName }}\
+    <span aria-hidden="true"> &times;</span>\
+    </button>',
+    props: ["fieldName"],
+  });
+
+  var cgpTextFilter = Vue.component("cgp-text-filter", {
+    template:
+      '<div>\
+      <h4>{{ fieldName }}</h4>\
+      <input v-model="inputValue" v-on:keyup.enter="addFilter()" :placeholder="fieldName">\
+      <div v-for="filter in filters" v-bind:key="filter" v-on:click="filters = filters.filter(e => e !== filter )"><cgp-pill :field-name="filter" /></div>\
+    </div>',
+    props: ["fieldName"],
+    data: function () {
+      return {
+        filters: [],
+        inputValue: "",
+      };
+    },
+    methods: {
+      addFilter: function () {
+        if (this.filters.indexOf(this.inputValue) === -1) {
+          this.filters.push(this.inputValue);
+        }
+        this.inputValue = "";
+      },
+    },
+  });
+
   var cgpFilters = Vue.component("cgp-filters", {
     template:
-      '<div class="pk-question">\
-			<h2>atts.question</h2>\
-			<button>Submit</button>\
+      '<div>\
+			<cgp-text-filter v-for="field in fields.text" v-bind:key="field" :field-name="field"/>\
 		</div>',
     data: function () {
       return {
-        selectedAnswer: null,
+        fields: {
+          text: ["Keywords", "Theme"],
+        },
       };
     },
-    methods: {},
   });
 
   var vm = new Vue({
     el: document.querySelector("#cgp-search-page"),
-    template:
-      '<div class="pk-container">\
+    template: "<div>\
+    <h2>Filters</h2>\
     <cgp-filters/>\
-    asdfasfdasfd\
-    </div>',
+    </div>",
     mounted: function () {
       console.log("Hello Vue!");
+    },
+    data: function () {
+      return {};
     },
   });
 })();
