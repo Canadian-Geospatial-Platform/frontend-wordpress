557eedb328effd09279a70efc543acb4da0b0bc0
Pascal Vautour <vautour.pascal@gmail.com>
2020-05-22 12:07:32 -0400

    href no longer jumps to top of page

--- wp-content/plugins/cgp-shortcodes/public/js/cgp-shortcodes-search-page.js
+++ wp-content/plugins/cgp-shortcodes/public/js/cgp-shortcodes-search-page.js
@@ -360,7 +360,7 @@
 
   var cgpPagingPageButton = Vue.component("cgp-paging-page-button", {
     template:
-      '<li :class="{active: active}" class="page-item" @click="setPage(value)"><a class="page-link" href="#">{{ value }}</a></li>',
+      '<li :class="{active: active}" class="page-item" @click="setPage(value)"><a class="page-link" href="#!">{{ value }}</a></li>',
     props: {
       active: {
         default: false,
@@ -384,20 +384,20 @@
       <div class="col-md-6 col-12 paging-big order-3 order-md-2">\
       <nav aria-label="Page navigation">\
       <ul class="pagination justify-content-center" style="list-style-type: none;">\
-      <li class="page-item" @click="setPage(page-1)"><a class="page-link" href="#"><</a></li>\
+      <li class="page-item" @click="setPage(page-1)"><a class="page-link" href="#!"><</a></li>\
       <cgp-paging-page-button v-show="page - a > 0" v-for="a in arrLeft" :value="page - a" v-on:setPage="setPage"/>\
       <cgp-paging-page-button active=true :value="page" v-on:setPage="setPage"/>\
       <cgp-paging-page-button v-show="page + a <= maxPage" v-for="a in arrRight" :value="page + a" v-on:setPage="setPage"/>\
-      <li class="page-item" @click="setPage(page+1)"><a class="page-link" href="#">></a></li>\
+      <li class="page-item" @click="setPage(page+1)"><a class="page-link" href="#!">></a></li>\
       </ul>\
       </nav>\
       </div>\
       <div class="col paging-small order-3">\
       <nav aria-label="Page navigation">\
       <ul class="pagination justify-content-center">\
-      <li class="page-item" @click="setPage(page-1)"><a class="page-link" href="#"><</a></li>\
+      <li class="page-item" @click="setPage(page-1)"><a class="page-link" href="#!"><</a></li>\
       <cgp-paging-page-button :active="true" :value="\'Page \' + page" v-on:setPage="setPage"/>\
-      <li class="page-item" @click="setPage(page+1)"><a class="page-link" href="#">></a></li>\
+      <li class="page-item" @click="setPage(page+1)"><a class="page-link" href="#!">></a></li>\
       </ul>\
       </nav>\
       </div>\
